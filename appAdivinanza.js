4ae1c1e4ac565cc615d1988fdce3890117645ed5
Fabiana Buchieri <141365085+FFabianaB@users.noreply.github.com>
2023-12-14 05:30:33 -0300

    arreglo parseo

--- appAdivinanza.js
+++ appAdivinanza.js
@@ -1,17 +1,17 @@
-const readlineSync = require('readline-sync');
+const readlineSync = require("readline-sync");
 
-const { generarNumeroAleatorio, verificarAdivinanza } = require('./adivinanza');
+const { generarNumeroAleatorio, verificarAdivinanza } = require("./adivinanza");
 const obtenerNumeroUsuario = () => {
-return readlineSync.question('Ingresa un número: ');
+  return parseInt(readlineSync.question("Ingresa un número: "));
 };
 const juegoAdivinanza = () => {
-const numeroSecreto = generarNumeroAleatorio();
-let numeroAdivinado = 0;
-console.log('¡Bienvenido a Adivina el número secreto!');
-console.log('Intenta adivinar el número del 1 al 100.\n');
-while (numeroAdivinado !== numeroSecreto) {
-numeroAdivinado = obtenerNumeroUsuario();
-verificarAdivinanza(numeroSecreto, numeroAdivinado);
-}
+  const numeroSecreto = generarNumeroAleatorio();
+  let numeroAdivinado = 0;
+  console.log("¡Bienvenido a Adivina el número secreto!");
+  console.log("Intenta adivinar el número del 1 al 100.\n");
+  while (numeroAdivinado !== numeroSecreto) {
+    numeroAdivinado = obtenerNumeroUsuario();
+    verificarAdivinanza(numeroSecreto, numeroAdivinado);
+  }
 };
 juegoAdivinanza();
